4fad2ffff23b62edad2d78f33c3c9336adf37fc2
jalvarez <61236470+snkalvarez@users.noreply.github.com>
2025-12-30 09:18:32 -0500

    build remove eureka

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ extra["springCloudVersion"] = "2025.0.0-RC1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("com.auth0:java-jwt:4.4.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
